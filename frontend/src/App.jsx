8c62024c09af119205fb410efe1ed276dac5336e
Tejassingh29 <tejas.singh@kalvium.community>
2025-01-29 11:54:09 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import { LoginPage, SignupPage } from './Routes.jsx'
-
-import './App.css';
-
-function App(){
-  return(
+import { BrowserRouter , Routes , Route} from "react-router-dom";
+import { LoginPage,SignupPage} from "./Routes.jsx";
+import './App.css' ;
+import Home from "./pages/Home.jsx";
+function App (){
+  return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignupPage/>}/>
-
-
-
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<Home />} />
+      
     </Routes>
     </BrowserRouter>
   );
