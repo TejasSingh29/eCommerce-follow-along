e33ad37f24f2a614b87904b7ab28ba63919a2f71
Tejassingh29 <tejas.singh@kalvium.community>
2025-02-25 15:17:30 +0530

    milestone 22 done

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -5,7 +6,7 @@ const User = require("../model/user");
 const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const catchAsyncErrors = require("../middleware/catchAsyncError");
 // const jwt = require("jsonwebtoken");
 // const sendMail = require("../utils/sendMail");
 const bcrypt = require("bcryptjs");
@@ -75,28 +76,48 @@ router.post(
     });
 }));
 
-router.get(
-  "/profile",
-  catchAsyncErrors(async (req, res, next) => {
-    const { email } = req.query;
-    if (!email) {
+router.get("/profile", catchAsyncErrors(async (req, res, next) => {
+  const { email } = req.query;
+  if (!email) {
       return next(new ErrorHandler("Please provide an email", 400));
-    }
-    const user = await User.findOne({ email });
-    if (!user) {
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
       return next(new ErrorHandler("User not found", 404));
-    }
-    res.status(200).json({
+  }
+  res.status(200).json({
       success: true,
       user: {
-        name: user.name,
-        email: user.email,
-        phoneNumber: user.phoneNumber,
-        avatarUrl: user.avatar.url,
+          name: user.name,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          avatarUrl: user.avatar.url
       },
       addresses: user.addresses,
-    });
-  })
-);
+  });
+}));
+
+router.post("/add-address", catchAsyncErrors(async (req, res, next) => {
+  const { country, city, address1, address2, zipCode, addressType, email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+      return next(new ErrorHandler("User not found", 404));
+  }
+  const newAddress = {
+      country,
+      city,
+      address1,
+      address2,
+      zipCode,
+      addressType,
+  };
+  user.addresses.push(newAddress);
+  await user.save();
+  res.status(201).json({
+      success: true,
+      addresses: user.addresses,
+  });
+}));
 
 module.exports = router;
